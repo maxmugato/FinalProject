00f018e1b219ddf0463c1fcedd86e2bae57e6362
maxmugato <114144181+maxmugato@users.noreply.github.com>
2024-05-13 15:00:37 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,276 @@
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+
+// Add static files location
+app.use(express.static("static"));
+
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
+
+
+
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+app.use(express.urlencoded({ extended: true }));
+
+
+const { Student } = require("./models/student");
+const { User } = require("./models/user");
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+
+// Set the sessions
+var session = require('express-session');
+app.use(session({
+  secret: 'secretkeysdfjsflyoifasd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+
+// Task 1 JSON formatted listing of students
+app.get("/all-students", function(req, res) {
+    var sql = 'select * from Students';
+    // As we are not inside an async function we cannot use await
+    // So we use .then syntax to ensure that we wait until the 
+    // promise returned by the async function is resolved before we proceed
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    });
+
+});
+
+// Task 2 display a formatted list of students
+app.get("/all-students-formatted", function(req, res) {
+    var sql = 'select * from Students';
+    db.query(sql).then(results => {
+    	    // Send the results rows to the all-students template
+    	    // The rows will be in a variable called data
+        res.render('all-students', {data: results});
+    });
+});
+
+// Single student page.  Show the students name, course and modules
+app.get("/student-single/:id", async function (req, res) {
+    var stId = req.params.id;
+    // Create a student class with the ID passed
+    var student = new Student(stId);
+    await student.getStudentName();
+    await student.getStudentProgramme();
+    await student.getStudentModules();
+    console.log(student);
+    res.render('student', {student:student});
+});
+
+
+
+// JSON output of all programmes
+app.get("/all-programmes", function(req, res) {
+    var sql = 'select * from Programmes';
+    // As we are not inside an async function we cannot use await
+    // So we use .then syntax to ensure that we wait until the 
+    // promise returned by the async function is resolved before we proceed
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    });
+
+});
+
+// Single programme page (no formatting or template)
+app.get("/programme-single/:id", async function (req, res) {
+    var pCode = req.params.id;
+    var pSql = "SELECT * FROM Programmes WHERE id = ?";
+    var results = await db.query(pSql, [pCode]);
+    //Now call the database for the modules
+    //Why do you think that the word modules is coming in before the name of the programme??
+    var modSql = "SELECT * FROM Programme_Modules pm \
+    JOIN Modules m on m.code = pm.module \
+    WHERE programme = ?";
+    var modResults = await db.query(modSql, [pCode]);
+    // String the results together, just for now.  Later we will push this
+    // through the template
+    res.send(JSON.stringify(results) + JSON.stringify(modResults));  
+});
+
+
+// Create a route for testing the db
+app.get("/db_test", function(req, res) {
+    // Assumes a table called test_table exists in your database
+    var sql = 'select * from test_table';
+    // As we are not inside an async function we cannot use await
+    // So we use .then syntax to ensure that we wait until the 
+    // promise returned by the async function is resolved before we proceed
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results)
+    });
+});
+
+// Create a route for /goodbye
+// Responds to a 'GET' request
+app.get("/goodbye", function(req, res) {
+    res.send("Goodbye world!");
+});
+
+
+
+// Create a dynamic route for /hello/<name>, where name is any value provided by user
+// At the end of the URL
+// Responds to a 'GET' request
+app.get("/hello/:name", function(req, res) {
+    // req.params contains any parameters in the request
+    // We can examine it in the console for debugging purposes
+    console.log(req.params);
+    //  Retrieve the 'name' parameter and use it in a dynamically generated page
+    res.send("Hello " + req.params.name);
+});
+
+app.post('/add-note', async function (req, res) {
+    params = req.body;
+    // Adding a try/catch block which will be useful later when we add to the database
+    var student = new Student(params.id);
+    try {
+         await student.addStudentNote(params.note);
+         res.send('form submitted');
+        }
+     catch (err) {
+         console.error(`Error while adding note `, err.message);
+     }
+     // Just a little output for now
+     res.send('form submitted');
+
+});
+
+
+
+app.post('/redirect', (req, res) => {
+    const { countryInput } = req.body;
+    // Handle redirection based on countryInput
+    // Redirect to appropriate routes (e.g., /about_russian, /about, /default_page)
+    switch (countryInput.toLowerCase()) {
+      case 'russia':
+        res.redirect('/about_russian.pug');
+        break;
+      case 'united kingdom':
+      case 'england':
+        res.redirect('/about.pug');
+        break;
+      default:
+        res.redirect('/default_page.pug');
+    }
+  });
+
+  // Register
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+  app.post('/set-password', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            // If a valid, existing user is found, set the password and redirect to the users single-student page
+            await user.setUserPassword(params.password);
+            console.log(req.session.id);
+            res.send('Password set successfully');
+        }
+        else {
+            // If no existing user is found, add a new one
+            newId = await user.addUser(params.email);
+            res.send('Perhaps a page where a new user sets a programme would be good here');
+        }
+    } catch (err) {
+        console.error(`Error while adding password `, err.message);
+    }
+});
+
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    console.log(req.session);
+    if (req.session.uid) {
+		res.send('Welcome back, ' + req.session.uid + '!');
+	} else {
+		res.send('Please login to view this page!');
+	}
+	res.end();
+});
+
+// Login
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            match = await user.authenticate(params.password);
+            if (match) {
+                req.session.uid = uId;
+                req.session.loggedIn = true;
+                console.log(req.session.id);
+                res.redirect('/about/');
+            }
+            else {
+                // TODO improve the user journey here
+                res.send('invalid password');
+            }
+        }
+        else {
+            res.send('invalid email');
+        }
+    } catch (err) {
+        console.error(`Error while comparing `, err.message);
+    }
+});  
+
+// Logout
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/login');
+  });
+
+app.get('/about', (req, res) => {
+    res.render('about'); 
+  });
+  
+  app.get('/calendar', (req, res) => {
+    res.render('calendar'); 
+  });
+
+app.get('/Contact', (req, res) => {
+    res.render('Contact'); 
+  });
+
+app.get('/reviews', (req, res) => {
+    res.render('reviews'); 
+  });
+
+app.get('/about_russian', (req, res) => {
+    res.render('about_russian');
+  });
+
+
+// Start server on port 3000
+app.listen(3000,function(){
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
